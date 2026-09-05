71a08db7acf90ee0a0a365cc6c1e416eab4de4e9
pgraczyk <pgraczyk.de@gmail.com>
2026-09-05 12:47:35 +0200

    created extract_video_data function

--- video_stats.py
+++ video_stats.py
@@ -52,11 +52,40 @@ def get_video_ids(playlist_id: str):
         except requests.exceptions.RequestException as e:
             raise e
 
+def extract_video_data(video_ids):
+    extracted_data = []
+
+    def batch_list(video_id_list, batch_size=MAX_RESULTS):
+        for video_id in range(0, len(video_id_list), batch_size):
+            yield video_id_list[video_id: video_id + batch_size]
+
+    try:
+        for batch in batch_list(video_ids):
+            video_id_str = ','.join(batch)
+            url = f'https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&part=snippet&part=statistics&id={video_id_str}&key={API_KEY}'
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+
+            for item in data.get('items', []):
+                video_data = {
+                    'video_id': item['id'],
+                    'title': item['snippet']['title'],
+                    'published_at': item['snippet']['publishedAt'],
+                    'duration': item['contentDetails']['duration'],
+                    'view_count': item['statistics'].get('viewCount', None),
+                    'like_count': item['statistics'].get('likeCount', None),
+                    'comment_count': item['statistics'].get('commentCount', None)
+                }
+                extracted_data.append(video_data)
+
+        return extracted_data
+
+    except requests.exceptions.RequestException as e:
+        raise e
 
 if __name__ == "__main__":      
     playlist_id = get_playlist_id()
     video_ids = get_video_ids(playlist_id)
-    #print(f"Playlist ID for channel '{CHANNEL_HANDLE}': {playlist_id}")
-    #print(f"Video IDs: {video_ids}")
-
-    
+    video_data = extract_video_data(video_ids)
+    print(video_data)
